907e38b6b18fd16de93f7342c8a18072e1e27f9d
Usman Nasution <usmannasution32@gmail.com>
2024-12-08 08:13:01 +0700

    Update config helepr

--- assets/js/config_helper.js
+++ assets/js/config_helper.js
@@ -1,16 +1,28 @@
 let CONFIG;
 
-const initConfig = next => {
-  if(CONFIG)
-    return next;
-  fetch('./config.json')
-  .then(r => r.json())
-  .then(config => next(config));
-}
+const initConfig = () => {
+  if (CONFIG) {
+    return Promise.resolve(CONFIG);
+  }
+  return fetch('./config.json')
+    .then(r => r.json())
+    .then(config => {
+      CONFIG = config;
+      return CONFIG;
+    });
+};
+
 const setConfigs = () => {
-  initConfig(config => {
+  initConfig().then(config => {
     const elements = document.querySelectorAll('[config]');
-    for(let element of elements)
+    for (let element of elements) {
       element.innerHTML = config[element.getAttribute('config')];
+    }
   });
+};
+
+const getConfig = async (keys) => {
+  let value = await initConfig();
+  keys.split('.').forEach(key => value = value[key]);
+  return value;
 };
