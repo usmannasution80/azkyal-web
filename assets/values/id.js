dac25f65f31099c3d7b9571ff99822ac183e144a
Usman Nasution <usmannasution32@gmail.com>
2024-12-09 21:50:49 +0700

    Add why us page

--- assets/values/id.js
+++ assets/values/id.js
@@ -1,88 +1,107 @@
 VALUES['id'] = {
-  "home" : "Home",
-  "about_us" : "Tentang Kami",
-  "cost" : "Tarif",
-  "contact" : "Kontak",
-  "start" : "Mulai",
-  "home_p" : "Solusi internet unlimited tampa batas.",
-  "featured_service_h1" : "Jaringan Stabil",
-  "featured_service_p1" : "Sangat cocok untuk anda yang hobi main game atau live streaming",
-  "featured_service_h2" : "Tarif Terjangkau",
-  "featured_service_p2" : "Tarif yang kami tetapkan sangat bersahabat.",
-  "featured_service_h3" : "Gratis Maintenance",
-  "featured_service_p3" : "Setiap kerusakan akan kami perbaiki tanpa dikenakan biaya apapun.",
-  "featured_service_h4" : "Biaya Pasang Murah",
-  "featured_service_p4" : "Biaya pasang kami cukup bersahabat, mulai Rp. 300.000 (tergantung domisili).",
-  "about_us_p" : "<b>PT. Azkyal Network Madina</b> bergerak di bidang penyedia jasa internet. Kami berupaya memberikan layanan internet yang berkualitas dan akan terus update sesuai dengan perkembangan era digital.",
-  "our_superiority" : "Keunggulan Kami",
-  "our_superiority_p" : "Kami selalu mengutamakan kenyamanan pelanggan.",
-  "our_superiority_1" : "Instalasi cepat",
-  "our_superiority_2" : "Perbaikan disegerakan",
-  "our_superiority_3" : "Gratis perbaikan dan penggantian alat",
-  "our_superiority_p2" : "Tidak perlu ragu untuk bergabung bersama kami!",
-  "private_user" : "Pelanggan Pribadi",
-  "ap_voucher" : "AP Voucher",
-  "admin_support" : "Admin Support",
-  "number_of_technicians" : "Jumlah Teknisi",
-  "monthly_prices_h" : "Tarif Bulanan",
-  "monthly_prices_p" : "Tarif yang kami tetapkan sangat bersahabat dan cocok bahkan jika anda masih seorang mahasiswa.",
-  "package" : "Paket :package",
-  "idr_cost" : "Rp. :cost",
-  "start_subscribe" : "Mulai Berlangganan",
-  "package_benefit_1" : "Harga stabil",
-  "package_benefit_2" : "Kuota unlimited",
-  "package_benefit_3" : "Kecepatan maksimal :speedmbps",
-  "package_benefit_4" : "Ideal untuk :device-count perangkat",
-  "package_benefit_5" : "Alat dipinjamkan",
-  "package_benefit_6" : "Gratis biaya pasang (syarat dan ketentuan berlaku)",
-  "package_benefit_7" : "Cocok untuk smart tv.",
-  "contact_p" : "Jika ada yang kurang jelas dan ingin ditanyakan, silahkan hubungi kami.",
-  "address" : "Alamat",
-  "contact_us" : "Kontak Kami",
-  "email_us" : "Email Kami",
-  "additional_services" : "Layanan Lainnya",
-  "additional_services_p" : "Kami juga menyediakan layanan lain yang mungkin menarik minat anda.",
-  "cctv_installation" : "Pemasangan CCTV",
-  "cctv_installation_p" : "Kami menyediakan layanan pemasangan cctv dengan tarif mulai dari Rp. 200.000 per titik (biaya di luar DVR).",
-  "wifi_voucher" : "WiFi Voucheran",
-  "wifi_voucher_p" : "Anda bisa menjadi reseller voucher wifi kami. Biaya pasang access point mulai dari Rp. 300.000. Anda akan mendapat satu akun voucher gratis.",
-  "affiliated_with" : "Bergabung Dengan",
-  "gallery" : "Galeri",
-  "all" : "Semua",
-  "maintenance" : "Maintenance",
-  "tools" : "Peralatan",
-  "gallery_items" : {
-    "tools" : [
+  'home' : 'Home',
+  'about_us' : 'Tentang Kami',
+  'cost' : 'Tarif',
+  'contact' : 'Kontak',
+  'start' : 'Mulai',
+  'home_p' : 'Solusi internet unlimited tampa batas.',
+  'featured_service_h1' : 'Jaringan Stabil',
+  'featured_service_p1' : 'Sangat cocok untuk anda yang hobi main game atau live streaming',
+  'featured_service_h2' : 'Tarif Terjangkau',
+  'featured_service_p2' : 'Tarif yang kami tetapkan sangat bersahabat.',
+  'featured_service_h3' : 'Gratis Maintenance',
+  'featured_service_p3' : 'Setiap kerusakan akan kami perbaiki tanpa dikenakan biaya apapun.',
+  'featured_service_h4' : 'Biaya Pasang Murah',
+  'featured_service_p4' : 'Biaya pasang kami cukup bersahabat, mulai Rp. 300.000 (tergantung domisili).',
+  'about_us_p' : '<b>PT. Azkyal Network Madina</b> bergerak di bidang penyedia jasa internet. Kami berupaya memberikan layanan internet yang berkualitas dan akan terus update sesuai dengan perkembangan era digital.',
+  'our_superiority' : 'Keunggulan Kami',
+  'our_superiority_p' : 'Kami selalu mengutamakan kenyamanan pelanggan.',
+  'our_superiority_1' : 'Instalasi cepat',
+  'our_superiority_2' : 'Perbaikan disegerakan',
+  'our_superiority_3' : 'Gratis perbaikan dan penggantian alat',
+  'our_superiority_p2' : 'Tidak perlu ragu untuk bergabung bersama kami!',
+  'private_user' : 'Pelanggan Pribadi',
+  'ap_voucher' : 'AP Voucher',
+  'admin_support' : 'Admin Support',
+  'number_of_technicians' : 'Jumlah Teknisi',
+  'monthly_prices_h' : 'Tarif Bulanan',
+  'monthly_prices_p' : 'Tarif yang kami tetapkan sangat bersahabat dan cocok bahkan jika anda masih seorang mahasiswa.',
+  'package' : 'Paket :package',
+  'idr_cost' : 'Rp. :cost',
+  'start_subscribe' : 'Mulai Berlangganan',
+  'package_benefit_1' : 'Harga stabil',
+  'package_benefit_2' : 'Kuota unlimited',
+  'package_benefit_3' : 'Kecepatan maksimal :speedmbps',
+  'package_benefit_4' : 'Ideal untuk :device-count perangkat',
+  'package_benefit_5' : 'Alat dipinjamkan',
+  'package_benefit_6' : 'Gratis biaya pasang (syarat dan ketentuan berlaku)',
+  'package_benefit_7' : 'Cocok untuk smart tv.',
+  'contact_p' : 'Jika ada yang kurang jelas dan ingin ditanyakan, silahkan hubungi kami.',
+  'address' : 'Alamat',
+  'contact_us' : 'Kontak Kami',
+  'email_us' : 'Email Kami',
+  'additional_services' : 'Layanan Lainnya',
+  'additional_services_p' : 'Kami juga menyediakan layanan lain yang mungkin menarik minat anda.',
+  'cctv_installation' : 'Pemasangan CCTV',
+  'cctv_installation_p' : 'Kami menyediakan layanan pemasangan cctv dengan tarif mulai dari Rp. 200.000 per titik (biaya di luar DVR).',
+  'wifi_voucher' : 'WiFi Voucheran',
+  'wifi_voucher_p' : 'Anda bisa menjadi reseller voucher wifi kami. Biaya pasang access point mulai dari Rp. 300.000. Anda akan mendapat satu akun voucher gratis.',
+  'affiliated_with' : 'Bergabung Dengan',
+  'gallery' : 'Galeri',
+  'all' : 'Semua',
+  'maintenance' : 'Maintenance',
+  'tools' : 'Peralatan',
+  'gallery_items' : {
+    'tools' : [
       {
-        "title" : "Splicer",
-        "description" : "Alat untuk menyambung fiber optic."
+        'title' : 'Splicer',
+        'description' : 'Alat untuk menyambung fiber optic.'
       },
       {
-        "title" : "Splicer",
-        "description" : "Alat untuk menyambung fiber optic."
+        'title' : 'Splicer',
+        'description' : 'Alat untuk menyambung fiber optic.'
       }
     ],
-    "maintenance" : [
+    'maintenance' : [
       {
-        "title" : "Perbaikan CCTV",
-        "description" : "Perbaikan CCTV yang bermasalah."
+        'title' : 'Perbaikan CCTV',
+        'description' : 'Perbaikan CCTV yang bermasalah.'
       },
       {
-        "title" : "Pemeliharaan Kabel",
-        "description" : "Pemeliharaan kabel fiber optic 6 core."
+        'title' : 'Pemeliharaan Kabel',
+        'description' : 'Pemeliharaan kabel fiber optic 6 core.'
       },
       {
-        "title" : "Pemasangan",
-        "description" : "Proses pemasangan kabel fiber optic 1 core."
+        'title' : 'Pemasangan',
+        'description' : 'Proses pemasangan kabel fiber optic 1 core.'
       },
       {
-        "title" : "Pemeliharaan Kabel",
-        "description" : "Momen istirahat saat memelihara kabel fiber optic 48 core."
+        'title' : 'Pemeliharaan Kabel',
+        'description' : 'Momen istirahat saat memelihara kabel fiber optic 48 core.'
       },
       {
-        "title" : "Pemeliharaan Kabel",
-        "description" : "Tampak roset sambungan kabel fiber optic 48 core."
+        'title' : 'Pemeliharaan Kabel',
+        'description' : 'Tampak roset sambungan kabel fiber optic 48 core.'
       }
     ]
-  }
+  },
+  'why_us' : 'Mengapa Kami?',
+  'about_us_p2' : 'PT. Azkyal Network Madina adalah perusahaan lokal pertama dan satu-satunya yang bergerak di bidang internet service provider yang berada di kabupaten Mandailing Natal.',
+  'about_us_p3' : 'PT. Azkyal Network Madina dalam menjalankan bisnis didukung oleh sumber daya manusia yang berkualitas dan berpengalaman dalam bidangnya maka anda jangan khawatir dengan pelayanan kami.',
+  'about_us_p4' : 'Kami yakin dengan sumber daya yang tinggi kami dapat menghasilkan kualitas pekerjaan dan pelayanan yang baik sesuai yang diharapkan.',
+  'about_us_p5' : 'Tujuan kami adalah menjadi pilihan utama bagi mitra bisnis kami dengan memberikan kontribusi kepada setiap klient kami, melebihi dari yang mereka harapkan, melalui pelayanan istimewa dari kami secara profesional dan integritas penuh.',
+  'vision' : 'Visi',
+  'vision_1' : 'Memberikan solusi internet hingga ke pelosok desa kabupaten Mandailing Natal',
+  'vision_2' : 'Terwujudnya jaringan internet untuk mendukung perkembangan teknologi digital di seluruh pelosok desa kabupaten Mandailing Natal',
+  'mission' : 'Misi',
+  'mission_1' : 'Memberikan standar baru layanan internet yang berkualitas',
+  'mission_2' : 'Menyediakan produk yang mendistribusikan internet dengan mudah sehingga mempercepat pemerataan internet ke seluruh pelosok desa di kabupaten Mandailing Natal',
+  'mission_3' : 'Memberikan akses informasi teknologi digital bagi masyarakat kabupaten Mandailing Natal',
+  'history' : 'Sejarah',
+  'history_p' : 'Inilah hasil pencapaian kami dari tahun ke tahun. Tanpa menyerah memberikan solusi akses internet ke pelosok desa di kabupaten Mandailing Natal.',
+  'history_2010_p' : 'Kami mengawali usaha dari sebuah warnet yang berdiri pada tahun 2010 di kabupaten Mandailing Natal. Warnet tersebut telah dikembangkan menjadi 3 cabang.',
+  'history_2019_p' : 'Memulai dari akar sebagai RTRWnet. Di tahun 2019, Azkyal mendapatkan izin distribusi internet oleh Provider JINOM (PT. Jinom Network Indonesia) dan berkomitmen untuk mendapatkan izin ISP sehingga bisnis bisa berkembang.',
+  'history_2022_p' : 'Azkyal berlisensi sebagai ISP dan berfokus membantu RTRWnet mendapatkan izin untuk melakukan jual kembali internet hingga berkembang memiliki reseller di Indonesia.',
+  'history_2023_p' : 'Kini, Azkyal berkomitmen melakukan pemasaran dan penjualan internet untuk menjadi perusahaan lokal pertama di kabupaten Mandailing Natal yang menyediakan list produk-produk internet dari berbagai penyedia layanan internet. ',
+  'vission_mission' : 'Visi & Misi'
 }
