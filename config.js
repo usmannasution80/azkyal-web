940c7ce6f9d614713bd3c6ceb4b845dbc8dec68c
Usman Nasution <usmannasution32@gmail.com>
2024-12-11 07:43:14 +0700

    Minor updates.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  "brand" : "PT. Azkyal Network Madina",
+  "brand" : "PT Azkyal Network Madina",
   "office_address" : "PT AZKYAL NETWORK MADINA<br/>Jl. Syekh abdul kadir mandili no.8, Kel. Kotasiantar, Kec. Panyabungan, Kab. Mandailing Natal - sumatera utara 22919",
   "admin_phone" : "+62 813-6044-9422",
   "offcie_email" : "pt.azkyalnetworkmadina@gmail.com",
